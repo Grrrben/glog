24380f48dc8405e0a25fe5f4a1edd663c1bed3f5
Grrrben <grrrben@gmail.com>
2018-02-25 13:59:55 +0100

    base

---
+++ glog.go
@@ -0,0 +1,80 @@
+package glog
+
+import (
+	"log"
+	"os"
+)
+
+const (
+	prefix_info    = "I "
+	prefix_warning = "W "
+	prefix_error   = "E "
+	prefix_fatal   = "F "
+)
+
+var isSetup bool = false
+
+var logfile string = "glog.log"
+
+func Setup(lf string) {
+	logfile = lf
+	isSetup = true
+}
+
+func getLog (prefix string) (*os.File) {
+	f, err := os.OpenFile(logfile, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+	if err != nil {
+		log.Fatalf("error opening file: %v", err)
+	}
+	log.SetOutput(f)
+	log.SetPrefix(prefix)
+	return f
+}
+
+func Info(line string) {
+	f := getLog(prefix_info)
+	defer f.Close()
+	log.Println(line)
+}
+
+func Infof(line string, vars ...interface{}) {
+	f := getLog(prefix_info)
+	defer f.Close()
+	log.Printf(line, vars...)
+}
+
+func Error(line string) {
+	f := getLog(prefix_error)
+	defer f.Close()
+	log.Println(line)
+}
+
+func Errorf(line string, vars ...interface{}) {
+	f := getLog(prefix_error)
+	defer f.Close()
+	log.Printf(line, vars...)
+}
+
+func Fatal(line string) {
+	f := getLog(prefix_fatal)
+	defer f.Close()
+	log.Println(line)
+}
+
+func Fatalf(line string, vars ...interface{}) {
+	f := getLog(prefix_fatal)
+	defer f.Close()
+	log.Printf(line, vars...)
+}
+
+func Warning(line string) {
+	f := getLog(prefix_warning)
+	defer f.Close()
+	log.Println(line)
+}
+
+func Warningf(line string, vars ...interface{}) {
+	f := getLog(prefix_warning)
+	defer f.Close()
+	log.Printf(line, vars...)
+}
