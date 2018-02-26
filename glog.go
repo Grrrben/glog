749fa96fb7ac5976c14963116e5093b1921293c1
Grrrben <grrrben@gmail.com>
2018-02-26 16:14:07 +0100

    SeverityLevel can now be set so Info and Warning can be muted

--- glog.go
+++ glog.go
@@ -5,6 +5,7 @@ import (
 	"os"
 )
 
+// Prefixes for the log lines to determine the severity of the message
 const (
 	prefix_info    = "I "
 	prefix_warning = "W "
@@ -12,15 +13,38 @@ const (
 	prefix_fatal   = "F "
 )
 
+// Exported constants for the severity level.
+// Use these when calling SetSeverityLevel(int)
+const (
+	Severity_level_error = 0
+	Severity_level_warning = 1
+	Severity_level_info    = 2
+)
+
+// Should everything be logged, or should Info and Warning be dismissed?
+var severityLevel int = Severity_level_info
+
 // this is where your log would be placed.
 var logfile string = "glog.log"
+
+// SetSeverityLevel tells glog to log or dismiss Warning and Info messages
+// To be used with the Severity_level_* constants
+func SetSeverityLevel(level int) {
+	if level < 0 || level > 2 {
+		// impossible value, should be consistent with the severity levels
+		severityLevel = Severity_level_info // include everything...
+	}
+	severityLevel = level
+}
 
 // SetLogFile sets a preferred path/and/file to the log.
 func SetLogFile(lf string) {
 	logfile = lf
 }
 
-func getLog (prefix string) (*os.File) {
+// getLog gives the caller a pointer to the log File so message strings can be added to the file
+// Should be closed after use.
+func getLog(prefix string) *os.File {
 	f, err := os.OpenFile(logfile, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
 	if err != nil {
 		log.Fatalf("error opening file: %v", err)
@@ -32,16 +56,20 @@ func getLog (prefix string) (*os.File) {
 
 // Info prints a line to the log beginning with the I char to clarify that it's an Info message
 func Info(msg string) {
-	f := getLog(prefix_info)
-	defer f.Close()
-	log.Println(msg)
+	if severityLevel >= Severity_level_info {
+		f := getLog(prefix_info)
+		defer f.Close()
+		log.Println(msg)
+	}
 }
 
 // Infof is identical to Info() but let's the user format the message string with any number of var's.
 func Infof(msg string, vars ...interface{}) {
-	f := getLog(prefix_info)
-	defer f.Close()
-	log.Printf(msg, vars...)
+	if severityLevel >= Severity_level_info {
+		f := getLog(prefix_info)
+		defer f.Close()
+		log.Printf(msg, vars...)
+	}
 }
 
 // Error prints a line to the log beginning with the E char to clarify that it's an Error message
@@ -75,16 +103,20 @@ func Fatalf(msg string, vars ...interface{}) {
 
 // Warning prints a line to the log beginning with the W char to clarify that it's an Warning message
 func Warning(msg string) {
-	f := getLog(prefix_warning)
-	defer f.Close()
-	log.Println(msg)
+	if severityLevel >= Severity_level_warning {
+		f := getLog(prefix_warning)
+		defer f.Close()
+		log.Println(msg)
+	}
 }
 
 // Warningf is identical to Warning() but let's the user format the message string with any number of var's.
 func Warningf(msg string, vars ...interface{}) {
-	f := getLog(prefix_warning)
-	defer f.Close()
-	log.Printf(msg, vars...)
+	if severityLevel >= Severity_level_warning {
+		f := getLog(prefix_warning)
+		defer f.Close()
+		log.Printf(msg, vars...)
+	}
 }
 
 // Flush is not yet implemented, needed for the API's interface sake
